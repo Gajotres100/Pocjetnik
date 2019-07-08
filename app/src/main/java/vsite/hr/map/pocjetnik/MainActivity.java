25f92fb35ba15d17f269dcb03ad405469643cbf6
Nikola Gaić <ngaic@combis.hr>
2019-07-08 16:17:58 +0200

    Dodan aktiviti

--- app/src/main/java/vsite/hr/map/pocjetnik/MainActivity.java
+++ app/src/main/java/vsite/hr/map/pocjetnik/MainActivity.java
@@ -14,26 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id==R.id.action_categories) {
-            Intent intent = new Intent(MainActivity.this, KategorijaActivity.class);
-            startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
