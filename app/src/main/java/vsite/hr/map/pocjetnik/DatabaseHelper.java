351ac793fe567579963796a5cb5670111b6e0571
Nikola Gaić <ngaic@combis.hr>
2019-07-08 15:42:46 +0200

    Initial commit

---
+++ app/src/main/java/vsite/hr/map/pocjetnik/DatabaseHelper.java
@@ -0,0 +1,47 @@
+package vsite.hr.map.pocjetnik;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import vsite.hr.map.pocjetnik.PocjetnikContract.KategorijaEntry;
+import vsite.hr.map.pocjetnik.PocjetnikContract.PocjetnikEntry;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "todosapp.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_CATEGORIES_CREATE=
+            "CREATE TABLE " + KategorijaEntry.TABLE_NAME + " (" +
+                    KategorijaEntry._ID + " INTEGER PRIMARY KEY, " +
+                    KategorijaEntry.COLUMN_DESCRIPTION + " TEXT " +
+                    ")";
+    private static final String TABLE_TODOS_CREATE =
+            "CREATE TABLE " + PocjetnikEntry.TABLE_NAME + " (" +
+                    PocjetnikEntry._ID + " INTEGER PRIMARY KEY, " +
+                    PocjetnikEntry.COLUMN_TEXT + " TEXT, " +
+                    PocjetnikEntry.COLUMN_CREATED + " TEXT default CURRENT_TIMESTAMP, " +
+                    PocjetnikContract.PocjetnikEntry.COLUMN_EXPIRED + " TEXT, " +
+                    PocjetnikEntry.COLUMN_DONE + " INTEGER, " +
+                    PocjetnikEntry.COLUMN_CATEGORY + " INTEGER, " +
+                    " FOREIGN KEY("+ PocjetnikEntry.COLUMN_CATEGORY + ") REFERENCES " +
+                    PocjetnikEntry.TABLE_NAME +
+                    "(" + PocjetnikEntry._ID +") " + ")";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CATEGORIES_CREATE);
+        db.execSQL(TABLE_TODOS_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + PocjetnikEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + KategorijaEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
