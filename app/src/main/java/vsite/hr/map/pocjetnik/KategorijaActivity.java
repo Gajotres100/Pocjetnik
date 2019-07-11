35b8ad1feb44e0ff9f42ec526898477b7fe684f4
Nikola Gaić <ngaic@combis.hr>
2019-07-11 14:48:23 +0200

    Dodan aktiviti

--- app/src/main/java/vsite/hr/map/pocjetnik/KategorijaActivity.java
+++ app/src/main/java/vsite/hr/map/pocjetnik/KategorijaActivity.java
@@ -1,4 +1,29 @@
 package vsite.hr.map.pocjetnik;
 
-public class KategorijaActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class KategorijaActivity extends AppCompatActivity {
+    String[] itemname ={
+            "Home",
+            "Work",
+            "Family",
+            "Sport",
+            "Hobbies"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kategorija_aktiviti);
+
+        final ListView lv=(ListView) findViewById(R.id.lvCategories);
+
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.kategorija_list_item,
+                R.id.tvNote,itemname));
+
+
+    }
 }
--- app/src/main/java/vsite/hr/map/pocjetnik/MainActivity.java
+++ app/src/main/java/vsite/hr/map/pocjetnik/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite) {
+            Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
