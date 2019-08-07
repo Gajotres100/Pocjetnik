c137204f4d21c2fe355bad2ccdc4cbc940a3f2a4
Nikola Gaić <ngaic@combis.hr>
2019-08-07 15:38:15 +0200

    Proba s providerom

--- app/src/main/java/vsite/hr/map/pocjetnik/Data/PocjetnikContract.java
+++ app/src/main/java/vsite/hr/map/pocjetnik/Data/PocjetnikContract.java
@@ -1,9 +1,17 @@
 package vsite.hr.map.pocjetnik.Data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class PocjetnikContract {
+
+    public static final String CONTENT_AUTHORITY = "vsite.hr.map.pocjetnik.pocjetnikprovider";
+    public static final String PATH_TODOS="todos";
+    public static final String PATH_CATEGORIES="categories";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
     public static final class PocjetnikEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TODOS);
 
         public static final String TABLE_NAME = "todos";
         public static final String _ID = BaseColumns._ID;
@@ -15,6 +23,8 @@ public class PocjetnikContract {
     }
 
     public static final class KategorijaEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_CATEGORIES);
 
         public static final String TABLE_NAME = "categories";
         public static final String _ID = BaseColumns._ID;
