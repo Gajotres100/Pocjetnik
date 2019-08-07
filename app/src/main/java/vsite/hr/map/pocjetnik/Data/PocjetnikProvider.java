1bdb06202f01151881c91c43a20079f8fe5a69c5
Nikola Gaić <ngaic@combis.hr>
2019-08-07 16:21:38 +0200

    Proba s providerom

--- app/src/main/java/vsite/hr/map/pocjetnik/Data/PocjetnikProvider.java
+++ app/src/main/java/vsite/hr/map/pocjetnik/Data/PocjetnikProvider.java
@@ -1,21 +1,90 @@
 package vsite.hr.map.pocjetnik.Data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
+import static vsite.hr.map.pocjetnik.Data.PocjetnikContract.CONTENT_AUTHORITY;
+import static vsite.hr.map.pocjetnik.Data.PocjetnikContract.PATH_CATEGORIES;
+import static vsite.hr.map.pocjetnik.Data.PocjetnikContract.PATH_TODOS;
+
+import vsite.hr.map.pocjetnik.Data.PocjetnikContract.KategorijaEntry;
+import vsite.hr.map.pocjetnik.Data.PocjetnikContract.PocjetnikEntry;
+
 public class PocjetnikProvider extends ContentProvider {
+    private static final int TODOS = 1;
+    private static final int TODOS_ID = 2;
+    private static final int CATEGORIES = 3;
+    private static final int CATEGORIES_ID = 4;
+
+    private static final UriMatcher uriMatcher =
+            new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(CONTENT_AUTHORITY, PATH_TODOS, TODOS);
+        uriMatcher.addURI(CONTENT_AUTHORITY, PATH_TODOS + "/#", TODOS_ID);
+        uriMatcher.addURI(CONTENT_AUTHORITY, PATH_CATEGORIES, CATEGORIES);
+        uriMatcher.addURI(CONTENT_AUTHORITY, PATH_CATEGORIES + "/#", CATEGORIES_ID);
+    }
+    private DatabaseHelper helper;
     @Override
     public boolean onCreate() {
-        return false;
+        helper = new DatabaseHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String orderBy) {
-        return null;
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = uriMatcher.match(uri);
+
+        String inTables = PocjetnikEntry.TABLE_NAME
+                + " inner join "
+                + KategorijaEntry.TABLE_NAME
+                + " on " + PocjetnikEntry.COLUMN_CATEGORY + " = "
+                + KategorijaEntry.TABLE_NAME + "." + KategorijaEntry._ID;
+        SQLiteQueryBuilder builder;
+        switch (match) {
+            case TODOS:
+                builder = new SQLiteQueryBuilder();
+                builder.setTables(inTables);
+                cursor = builder.query(db, projection, null, null, null, null, orderBy);
+                break;
+            case TODOS_ID:
+                builder = new SQLiteQueryBuilder();
+                builder.setTables(inTables);
+                selection = PocjetnikEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                cursor = builder.query(db, projection, selection, selectionArgs, null, null, orderBy);
+                break;
+            case CATEGORIES:
+                cursor = db.query(KategorijaEntry.TABLE_NAME,
+                        projection, null, null, null, null, orderBy);
+                break;
+            case CATEGORIES_ID:
+                selection = KategorijaEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                cursor = db.query(KategorijaEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, orderBy);
+                break;
+            default:
+                throw new IllegalArgumentException("Query unknown URI: " + uri);
+
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Nullable
